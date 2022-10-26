2f776b1f194f396a6d43426304e086f454569250
OTAVIO NICOLAU DA SILVA <otavio.nicollau@gmail.com>
2022-10-26 12:07:20 -0300

    Update index.js
    AJUSTE  22

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function Home() {
 
                     {concursos.cursos.map((cursos) => (
                       
-                        <div key={cursos.id} className="d-grid gap-2" >
+                        <div key={cursos.id} >
                           <Button variant="link">
                               <Link href={`/curso/${cursos.id}`}><a> {cursos.nome}  {' ( '} {cursos.total_aulas} {cursos.total_aulas > 1 ? <a>Aulas</a> : <a>Aula</a>} {' ) '}</a></Link>
                           </Button>
